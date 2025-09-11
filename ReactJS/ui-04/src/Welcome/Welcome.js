1884ac9e837d2bb898315feede4345eb16377109
virkar sangita <sangitavirkar2205@gmail.com>
2025-09-11 18:22:08 +0530

    create react demo

---
+++ ReactJS/ui-04/src/Welcome/Welcome.js
@@ -0,0 +1,9 @@
+
+import './Welcome.css'
+function Welcome(){
+    return(
+        <h1 className="demo">Welcome To Developer Guru</h1>
+    )
+}
+
+export default Welcome;
--- ReactJS/ui-04/src/index.js
+++ ReactJS/ui-04/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Welcome  from './Welcome/Welcome';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <Welcome/>
   </React.StrictMode>
 );
 
